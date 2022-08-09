4ffc28d232eeeb74fab41ace00d0b9107ed7add8
lavi <62757958+lavi27@users.noreply.github.com>
2022-08-10 02:44:00 +0900

    add : 체널 선택 기능 완성

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -16,6 +16,7 @@
         <input
           class="input-s"
           :value="input.prefix"
+          @input="input.prefix = $event.target.value"
           placeholder="!"
           type="text"
         />
@@ -42,18 +43,26 @@
         <input
           class="input-l"
           :value="input.channel"
+          @click="
+            list.channel.show = true;
+            setChannelInput($event.target.value);
+            searchChannel($event.target.value.substr(2));
+          "
+          @input="
+            setChannelInput($event.target.value);
+            searchChannel($event.target.value.substr(2));
+          "
+          v-click-outside="onClickOutside"
           placeholder="# 체널-이름"
           type="text"
         />
-        <ul>
-          <li value="직업선택"># 직업선택</li>
-          <li value="학생"># 학생</li>
-          <li value="회사원"># 회사원</li>
-          <li value="기타"># 기타</li>
-          <li value="기타"># 기타</li>
-          <li value="기타"># 기타</li>
-          <li value="기타"># 기타</li>
-          <li value="기타"># 기타</li>
+        <ul v-if="list.channel.show">
+          <li
+            v-for="index in list.channel.list.length"
+            @click="
+              setChannelInput('# ' + list.channel.list[index - 1]);
+            "
+          ># {{list.channel.list[index - 1]}}</li>
         </ul>
       </div>
     </form>
@@ -61,7 +70,64 @@
 </template>
 
 <script>
+import vClickOutside from 'v-click-outside'
+
+const exampleValue = [
+  '직업선택',
+  '학생',
+  '회사원',
+  '기타',
+  '기타',
+  '기타',
+  '기타',
+  '기타'
+].sort();
+
+// https://taegon.kim/archives/9919
+function ch2pattern(ch) {
+  const offset = 44032;
+
+  if (/[가-힣]/.test(ch)) {
+    const chCode = ch.charCodeAt(0) - offset;
+
+    if (chCode % 28 > 0) {
+      return ch;
+    }
+    const begin = Math.floor(chCode / 28) * 28 + offset;
+    const end = begin + 27;
+    return `[\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+  }
+
+  if (/[ㄱ-ㅎ]/.test(ch)) {
+    const con2syl = {
+      'ㄱ': '가'.charCodeAt(0),
+      'ㄲ': '까'.charCodeAt(0),
+      'ㄴ': '나'.charCodeAt(0),
+      'ㄷ': '다'.charCodeAt(0),
+      'ㄸ': '따'.charCodeAt(0),
+      'ㄹ': '라'.charCodeAt(0),
+      'ㅁ': '마'.charCodeAt(0),
+      'ㅂ': '바'.charCodeAt(0),
+      'ㅃ': '빠'.charCodeAt(0),
+      'ㅅ': '사'.charCodeAt(0),
+    };
+    const begin = con2syl[ch] || ( ( ch.charCodeAt(0) - 12613 ) * 588 + con2syl['ㅅ'] );
+    const end = begin + 587;
+    return `[${ch}\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+  }
+
+  return escapeRegExp(ch);
+}
+
+function createFuzzyMatcher(input) {
+  const pattern = input.split('').map(ch2pattern).join('.*?');
+  return new RegExp(pattern);
+}
+
 export default {
+  created() {
+    this.list.channel.list = exampleValue;
+  },
   data() {
     return {
       input: {
@@ -71,11 +137,38 @@ export default {
       switch_: {
         bot_prefix: false,
       },
+      list: {
+        channel: {
+          show: false,
+          list: []
+        }
+      }
     }
+  },
+  directives: {
+    clickOutside: vClickOutside.directive,
   },
   methods: {
     inputSwitch: function (name) {
-      this.switch_[name] = !this.switch_[name]
+      this.switch_[name] = !this.switch_[name];
+    },
+    setChannelInput: function (value) {
+      this.input.channel = '';
+      this.input.channel = '# ' + value.substr(2);
+    },
+    searchChannel: function (value) {
+      this.list.channel.list = [];
+
+      exampleValue.forEach(name => {
+        if(!createFuzzyMatcher(value).test(name)) {
+          return;
+        }
+
+        this.list.channel.list.push(name);
+      });
+    },
+    onClickOutside() {
+      this.list.channel.show = false
     },
   },
 }
