52bb309a9bdfdf8d945f1ad2bceed54867554f2a
lavi <62757958+lavi27@users.noreply.github.com>
2022-08-12 04:52:23 +0900

    fix : navbar가 지랄나는 버그 수정

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,7 @@
       <div class="fixed top-0 left-0 w-full h-full bg-black/[0.5] z-10" v-if="showNav && isMobile">a</div>
     </transition>
 
-    <div style="background: #151720" class="relative w-full h-full z-20">
+    <div style="background: #151720; min-height: calc(100vh - 72px)" class="relative w-full h-full z-20">
       <div class="sidebar flex flex-col shrink-0 w-40 lg:w-64 md:w-56 h-full ml-0 lg:ml-36 p-2.5 md:p-4 select-none">
         <div @click="showNav = !showNav" class="menuIcon">
           <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m22 16.75c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/></svg>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col w-full" style="background: #0f1016">
+  <div class="flex flex-col w-full min-h-full" style="background: #0f1016">
     <Header />
 
     <div class="bottom_content flex w-full">
