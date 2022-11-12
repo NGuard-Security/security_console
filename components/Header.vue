d05f2000d6156cb2b3d67a737df4b80dbe8aac20
Minsu Kim <minsu_kim@bishanoi.net>
2022-11-13 00:56:58 +0700

    feat: UX 개선

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,9 @@
   <header class="fixed w-full text-sm bg-black z-30">
     <div class="headerContent flex items-center lg:justify-between max-w-screen-xl mx-auto p-4 text-gray-200">
       <div style="height: 40px" class="logoImg mr-auto md:mr-0">
-        <img src="~/assets/img/logo1.png" alt="NGuard logo" class="h-full" />
+        <a href="https://nguard.xyz" class="nav_item">
+          <img src="~/assets/img/logo1.png" alt="NGuard logo" class="h-full" />
+        </a>
       </div>
 
       <transition appear name="nav" mode="out-in">
