5954ab8b6af83fc9af2e473a36b28f11fac5c50f
Oracle <kms0219kms@naver.com>
2023-12-31 12:20:30 +0000

    :rocket: chore: v2 출시 준비 (테스트 환경 코드 삭제)

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -456,7 +456,7 @@ export default {
   async mounted() {
     window.addEventListener('resize', this.resizeAlerts)
 
-    const socket = io('https://api-v2test.nguard.dev/')
+    const socket = io('https://api.nguard.xyz/')
 
     socket.on('push:load', pushs => {
       this.alerts.contents = pushs
