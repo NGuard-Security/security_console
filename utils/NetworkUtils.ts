252dbaa5297039f00f78a663065348ff4b933965
lavi <62757958+lavi27@users.noreply.github.com>
2024-01-15 04:00:07 +0900

    add: 에러 로깅 향상

--- utils/NetworkUtils.ts
+++ utils/NetworkUtils.ts
@@ -1,24 +1,28 @@
 import type { AxiosError } from 'axios'
 
 const catchNetworkErr = (e: AxiosError<any, any>, doAlert: boolean = false) => {
-  if (e.config?.url) console.error('NetworkErrorURL: ' + e.config.url)
-  console.error(e.name)
-  console.error(e.message)
+  let errMsg = ''
 
   if (e.response) {
-    console.error('NetworkError: Response received')
+    errMsg = 'Response received'
     console.error(e.response.data)
     console.error(e.response.status)
     console.error(e.response.headers)
   } else if (e.request) {
-    console.error('NetworkError: Response not received, it could be CORS issue')
+    errMsg = 'Response not received, it could be CORS issue'
     console.error(e.request)
   } else {
-    console.error('NetworkError: Request not made')
+    errMsg = 'Request not made'
     console.error(e.message)
   }
 
-  if (doAlert) alert('통신 중 오류가 발생하였습니다. 채널톡으로 문의해 주세요.')
+  console.error(`[NETWORK ERROR]
+ErrorType:    ${errMsg}
+ErrorName:    ${e.name}
+ErrorMessage: ${e.message}
+${e.config?.url ? `ErrorURL:     ${e.config.url}` : ''}`)
+
+  if (doAlert) alert('통신 중 오류가 발생하였습니다. 채널톡으로 문의 부탁드립니다.')
 }
 
 export { catchNetworkErr }
