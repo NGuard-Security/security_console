b440a9e44978463bea9cb56c844adc7d9fe1fee3
lavi <62757958+lavi27@users.noreply.github.com>
2024-01-15 02:37:48 +0900

    fix: spinner 나타나지 않는 에러 해결

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-const props = defineProps<{ type: number }>()
+const { type } = defineProps<{ type: number }>()
+if (!type) throw new Error(`LoadingSpinner component의 prop인 'type'이 누락되었습니다.`)
 </script>
 
 <template>
-  <div @class=";`spinner${props}`"></div>
+  <div :class="'spinner' + type"></div>
 </template>
 
 <style lang="scss">
