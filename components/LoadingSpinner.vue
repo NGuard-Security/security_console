8e9913ca23009686d96004e2e2218878b3652434
lavi <62757958+lavi27@users.noreply.github.com>
2024-01-18 01:10:51 +0900

    chore: 파일 정리

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -7,36 +7,6 @@ if (!type) throw new Error(`LoadingSpinner component의 prop인 'type'이 누락
   <div :class="'spinner' + type"></div>
 </template>
 
-<style lang="scss">
-.spinner1 {
-  color: $color-gray;
-  display: inline-block;
-  width: 50px;
-  height: 50px;
-  vertical-align: -0.125em;
-  border: 0.25em solid;
-  border-right: 0.25em solid transparent;
-  border-radius: 50%;
-  -webkit-animation: spinner 0.75s linear infinite;
-  animation: spinner 0.75s linear infinite;
-}
-
-.spinner2 {
-  color: $color-white;
-  display: inline-block;
-  width: 2rem;
-  height: 2rem;
-  vertical-align: -0.125em;
-  border: 0.25em solid;
-  border-right: 0.25em solid transparent;
-  border-radius: 50%;
-  -webkit-animation: spinner 0.75s linear infinite;
-  animation: spinner 0.75s linear infinite;
-}
-
-@keyframes spinner {
-  to {
-    transform: rotate(1turn);
-  }
-}
+<style lang="scss" scoped>
+@import url('~/assets/styles/components/loadingSpinner.scss');
 </style>
