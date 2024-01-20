d1a2d63caf1f6bdb47ae26e3d510ff8dd1f325f3
lavi <62757958+lavi27@users.noreply.github.com>
2024-01-21 00:10:51 +0900

    feat: Nuxt GTM 종속성 기본설정 및 레거시 삭제

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,10 +21,6 @@ export default defineNuxtConfig({
       ],
       script: [
         {
-          // Google Tag Manager
-          textContent: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KKQTHLK');`,
-        },
-        {
           // MS Clarity
           textContent: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "kdxoqws6v8");`,
         },
@@ -41,12 +37,6 @@ export default defineNuxtConfig({
         {
           nonce: 'QHzY_FUE44g8R4HbPxq60A',
           textContent: `;(function () {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe');iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';iframe.style.display = 'none';iframe.name = 'googlefcPresent';document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})()`,
-        },
-      ],
-      noscript: [
-        {
-          // Google Tag Manager (noscript)
-          textContent: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KKQTHLK" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         },
       ],
       link: [
@@ -119,7 +109,28 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxtjs/svg-sprite',
     '@nuxtjs/tailwindcss',
+    '@zadigetvoltaire/nuxt-gtm',
   ],
+
+  gtm: {
+    id: 'GTM-KKQTHLK',
+    // queryParams: {
+    //   // Add URL query string when loading gtm.js with GTM ID (required when using custom environments)
+    //   gtm_auth: 'AB7cDEf3GHIjkl-MnOP8qr',
+    //   gtm_preview: 'env-4',
+    //   gtm_cookies_win: 'x',
+    // },
+    // defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
+    // compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
+    // nonce: '2726c7f26c', // Will add `nonce` to the script tag
+    // enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+    // debug: true, // Whether or not display console logs debugs (optional)
+    // loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+    // enableRouterSync: true, // Pass the router instance of your app to automatically sync with router (optional)
+    // ignoredViews: ['homepage'], // Don't trigger events for specified router names (optional)
+    // trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
+    // devtools: true, // (optional)
+  },
 
   svgSprite: {
     input: '~/assets/icons',
