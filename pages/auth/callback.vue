08f8059ab89c3e5471a425c6a08729a481ac5c84
lavi <62757958+lavi27@users.noreply.github.com>
2024-01-21 01:50:02 +0900

    dump

--- pages/auth/callback.vue
+++ pages/auth/callback.vue
@@ -1,49 +1,30 @@
 <template>
-  <div
-    class="overflow-x-hidden servers-wrap flex items-center justify-center flex-col w-full h-full min-h-screen px-10"
-  >
-    <h1 class="w-fit text-3xl font-bold mx-auto mb-14 text-white">
-      <!-- 로그인 -->
-      {{ $t('common.auth.login.title') }}
-    </h1>
+  <NuxtLayout name="app-base">
+    <div class="overflow-x-hidden servers-wrap flex items-center justify-center flex-col w-full h-full px-10">
+      <h1 class="w-fit text-3xl font-bold mx-auto mb-14 text-white">
+        <!-- 로그인 -->
+        {{ $t('common.auth.login.title') }}
+      </h1>
 
-    <div class="relative w-full flex justify-center mb-20">
-      <!-- 스피너 -->
-      <transition name="spiner">
-        <div id="loading" class="absolute mx-auto">
-          <div class="text-center">
-            <div class="spinner"></div>
-            <h4 class="text-base pt-5 text-[#999999]">
-              <!-- 로그인 정보를 불러오는 중입니다... -->
-              {{ $t('common.auth.login.loading') }}
-            </h4>
+      <div class="relative w-full flex justify-center mb-20">
+        <!-- 스피너 -->
+        <transition name="spiner">
+          <div id="loading" class="absolute mx-auto">
+            <div class="text-center">
+              <div class="spinner"></div>
+              <loading-spinner :type="1" />
+
+              <h4 class="text-base pt-5 text-[#999999]">
+                <!-- 로그인 정보를 불러오는 중입니다... -->
+                {{ $t('common.auth.login.loading') }}
+              </h4>
+            </div>
           </div>
-        </div>
-      </transition>
+        </transition>
+      </div>
     </div>
-  </div>
+  </NuxtLayout>
 </template>
-
-<style lang="scss" scoped>
-.spinner {
-  color: $color-gray;
-  display: inline-block;
-  width: 50px;
-  height: 50px;
-  vertical-align: -0.125em;
-  border: 0.25em solid;
-  border-right: 0.25em solid transparent;
-  border-radius: 50%;
-  -webkit-animation: spinner 0.75s linear infinite;
-  animation: spinner 0.75s linear infinite;
-}
-
-@keyframes spinner {
-  to {
-    transform: rotate(1turn);
-  }
-}
-</style>
 
 <script setup lang="ts">
 const API = useAPI()
